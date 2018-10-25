da49dcbba4b24e7a39f9ca83edfdb6f17ebb0963
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-10-25 12:14:36 +0700

    update test case

--- test/token_transfer.js
+++ test/token_transfer.js
@@ -1,27 +1,26 @@
-const assert = require('assert')
-const TokenManger = artifacts.require('./TokenManager.sol');
-const TweentyScoopsToken = artifacts.require('./TweentyScoopsToken.sol');
+const assert = require("assert");
+const TokenManger = artifacts.require("./TokenManager.sol");
+const TweentyScoopsToken = artifacts.require("./TweentyScoopsToken.sol");
 
 const BigNumber = web3.BigNumber;
 let tokenManager, tweentyScoopsToken;
 
-contract('token_management', async (accounts) => {
+contract("token_management", async accounts => {
+  beforeEach(async () => {
+    tweentyScoopsToken = await TweentyScoopsToken.new();
+    tokenManager = await TokenManger.new(tweentyScoopsToken.address);
+  });
 
-    let accountA, accountB;
-
-    [accountA, accountB] = accounts;
-
-    beforeEach(async () => {
-        tokenManager = await TokenManger.new();
-        tweentyScoopsToken = await TweentyScoopsToken.new();
-        await tokenManager.addNewToken('SC', tweentyScoopsToken.address);
-    });
-
-    it("should be able to transfer sender token to another wallet", async() => {
-        let amount = new BigNumber(50000000000);
-        await tweentyScoopsToken.approve(tokenManager.address, amount,{from: accountA});
-        await tokenManager.transferTokens('SC',accountB, amount,{from: accountA});
-        let balance = ((await  tweentyScoopsToken.balanceOf(accountB)).toString());
-        assert.equal(balance, amount.toString())
-    });
+  it("should be able to transfer sender token to another wallet", async () => {
+    let amount = new BigNumber(999);
+    const txApprove = await tweentyScoopsToken.approve(
+      tokenManager.address,
+      amount
+    );
+    assert.ok(txApprove);
+    const txTransfer = await tokenManager.transferTokens(accounts[1], amount);
+    assert.ok(txTransfer);
+    let balance = (await tweentyScoopsToken.balanceOf(accounts[1])).toString();
+    assert.equal(balance, amount.toString());
+  });
 });
